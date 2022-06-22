8975cf3be1e0382f4bb3d0da2aeb6e0ab6993fc4
vvenommm <96784333+vvenommm@users.noreply.github.com>
2022-06-22 17:01:57 +0900

    Create for01.py

---
+++ 0622_Intro/for01.py
@@ -0,0 +1,19 @@
+arr = ["널자바", "김루피"]
+print(arr)
+
+arr.append("솜클레어")
+print(arr)
+
+arr.insert(0, "잭스흐")
+print(arr)
+
+arr.insert(2, "은루씨")
+print(arr)
+
+arr.insert(len(arr), "최조이")
+print(arr)
+
+'''
+    insert만으로도 큐와 스택의 기능을 다 쓸 수 있다.
+    insert(len(배열명), "값") = 배열명.append("값")
+'''

d4dcfc546a74408ab4115213b558a268cb74c29b
vvenommm <96784333+vvenommm@users.noreply.github.com>
2022-06-22 16:19:37 +0900

    Update 0622_mytest01.py

--- 0622_Intro/0622_mytest01.py
+++ 0622_Intro/0622_mytest01.py
@@ -3,11 +3,17 @@
     두번째 숫자 입력
     두 수의 합은 n입니다.
 """
-
-from tkinter.tix import INTEGER
-
-
+'''
 a = input("숫자를 입력하세요")
 b = input("숫자를 또 입력하세요")
 c = int(a) + int(b)
 print("두 수의 합은 " + str(c) +"입니다.")
+'''
+
+a = input("숫자를 입력하세요")
+b = input("숫자를 또 입력하세요")
+aa = int(a)
+bb = int(b)
+sum = aa+bb
+print("{}과 {}의 합은 {}입니다.".format(aa,bb,sum))
+

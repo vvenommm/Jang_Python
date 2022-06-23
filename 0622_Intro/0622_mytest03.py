612f57d1986083019577728c1bce0cde62811fe0
vvenommm <96784333+vvenommm@users.noreply.github.com>
2022-06-23 15:00:37 +0900

    Update 0622_mytest03.py

--- 0622_Intro/0622_mytest03.py
+++ 0622_Intro/0622_mytest03.py
@@ -14,3 +14,27 @@ if answer == mypick :
     print("결과 : {}!\nyou win!".format(answer))
 else : 
     print("결과 : {}!\nyou lose!".format(answer))
+
+#########################################################
+
+com=""
+mine = ""
+result = ""
+
+mine = input("홀? 짝?")
+rnd = random()
+print("rnd", rnd)
+
+if rnd > 0.5:
+    com = "홀"
+else:
+    com = "짝"
+
+if mine == com:
+    result = "이김"
+else:
+    result = "짐"
+    
+print("com", com)
+print("mine", mine)
+print("result", result)

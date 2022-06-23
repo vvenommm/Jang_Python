1921b1ebd1e892ad989c542ba123fa534b22e5f5
vvenommm <96784333+vvenommm@users.noreply.github.com>
2022-06-23 15:14:55 +0900

    Create mytest04.py

---
+++ day02/mytest04.py
@@ -0,0 +1,18 @@
+# 1에서 9 사이의 정수 중 3가지를 중복 없이 랜덤으로 출력하세요
+from random import random
+
+arr = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+print("원래 배열 : {}".format(arr))
+
+temp = arr[0]
+arr[0] = arr[1]
+arr[1] = temp
+print("1과 2만 섞은 후 : {}".format(arr))
+
+for i in range(9):
+    rnd = int(random()*9)
+    temp = arr[i]
+    arr[i] = arr[rnd]
+    arr[rnd] = temp
+    
+print("랜덤으로 섞은 후 : {}".format(arr))

ec34e6e25db6a2d6c9f3a271bb59a1a6c4478481
vvenommm <96784333+vvenommm@users.noreply.github.com>
2022-06-23 15:50:11 +0900

    Create mydef01.py

---
+++ day02/mydef01.py
@@ -0,0 +1,27 @@
+def add(a,b):
+    return a+b;
+
+sum = add(5, 8)
+print("sum", sum)
+
+def minus(a, b):
+    return a-b;
+
+min = minus(5, 8)
+print("min", min)
+
+def multiply(a, b):
+    return a*b;
+
+mul = multiply(5, 8)
+print("mul", mul)
+
+def divide(a, b):
+    return a/b;
+div = divide(5, 8);
+print("div", div)
+
+def mod(a, b):
+    return a%b;
+mod = mod(5, 8)
+print("mod", mod)

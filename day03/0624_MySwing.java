d38a116bc1fa55209b3a4df69529293939f546d5
vvenommm <96784333+vvenommm@users.noreply.github.com>
2022-06-24 17:29:24 +0900

    Create 0624_MySwing.java

---
+++ day03/0624_MySwing.java
@@ -0,0 +1,13 @@
+package day03;
+
+import javax.swing.JFrame;
+
+public class MySwing {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		JFrame f = new JFrame();
+		f.setSize(600, 600);
+		f.setVisible(true);
+	}
+}

8e76873308205680de4eed12db0aa94815ee4c37
vvenommm <96784333+vvenommm@users.noreply.github.com>
2022-06-24 17:30:46 +0900

    Update 0624_MySwing02.java

--- day04/0624_MySwing02.java
+++ day04/0624_MySwing02.java
@@ -1,24 +1,19 @@
 package day04;
 
-import java.awt.BorderLayout;
 import java.awt.EventQueue;
+import java.awt.Font;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JLabel;
-import javax.swing.JTextField;
 import javax.swing.JButton;
-import java.awt.Font;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
 
-public class MySwing05 extends JFrame {
+public class MySwing02 extends JFrame {
 
 	private JPanel contentPane;
-	private JTextField tf_mine;
-	private JTextField tf_com;
-	private JTextField tf_result;
 
 	/**
 	 * Launch the application.
@@ -27,7 +22,7 @@ public class MySwing05 extends JFrame {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					MySwing05 frame = new MySwing05();
+					MySwing02 frame = new MySwing02();
 					frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
@@ -39,7 +34,7 @@ public class MySwing05 extends JFrame {
 	/**
 	 * Create the frame.
 	 */
-	public MySwing05() {
+	public MySwing02() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
@@ -47,66 +42,25 @@ public class MySwing05 extends JFrame {
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-		JLabel lbl_mine = new JLabel("나");
-		lbl_mine.setFont(new Font("굴림", Font.PLAIN, 20));
-		lbl_mine.setBounds(52, 28, 52, 15);
-		contentPane.add(lbl_mine);
+		JLabel lbl = new JLabel("100");
+		lbl.setFont(new Font("굴림", Font.PLAIN, 30));
+		lbl.setBounds(78, 125, 76, 35);
+		contentPane.add(lbl);
 		
-		JLabel lbl_com = new JLabel("com");
-		lbl_com.setFont(new Font("굴림", Font.PLAIN, 20));
-		lbl_com.setBounds(52, 84, 52, 15);
-		contentPane.add(lbl_com);
-		
-		JLabel lbl_result = new JLabel("결과");
-		lbl_result.setFont(new Font("굴림", Font.PLAIN, 20));
-		lbl_result.setBounds(52, 138, 52, 30);
-		contentPane.add(lbl_result);
-		
-		tf_mine = new JTextField();
-		tf_mine.setFont(new Font("굴림", Font.PLAIN, 20));
-		tf_mine.setBounds(185, 10, 138, 45);
-		contentPane.add(tf_mine);
-		tf_mine.setColumns(10);
-		
-		tf_com = new JTextField();
-		tf_com.setFont(new Font("굴림", Font.PLAIN, 20));
-		tf_com.setBounds(185, 68, 138, 46);
-		contentPane.add(tf_com);
-		tf_com.setColumns(10);
-		
-		tf_result = new JTextField();
-		tf_result.setFont(new Font("굴림", Font.PLAIN, 20));
-		tf_result.setBounds(166, 138, 157, 45);
-		contentPane.add(tf_result);
-		tf_result.setColumns(10);
-		
-		JButton btn = new JButton("게임하기");
-		btn.setFont(new Font("굴림", Font.PLAIN, 20));
-		btn.setBounds(52, 201, 120, 40);
+		JButton btn = new JButton("increase");
+		btn.setFont(new Font("굴림", Font.PLAIN, 30));
+		btn.setBounds(215, 125, 169, 39);
 		contentPane.add(btn);
 		
 		btn.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
-				hj();
+				// TODO Auto-generated method stub
+				super.mouseClicked(e);
+				lbl.setText((Integer.parseInt(lbl.getText())+100) + "");
 			}
 		});
-	}
-	
-	public void hj() {
-		int rnd = (int)(Math.random()*100+1);
 		
-		if(rnd%2 == 0) {
-			tf_com.setText("짝");
-		}else {
-			tf_com.setText("홀");
-		}
 		
-		if(tf_com.getText().equals(tf_mine.getText())) {
-			tf_result.setText("\"나\"승리!");
-		}else {
-			tf_result.setText("\"컴퓨터\"승리!");
-		}
 	}
-
 }

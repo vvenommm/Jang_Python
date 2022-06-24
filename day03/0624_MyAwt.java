f30b3256ad05dfdae86cb061f1032e489de999ac
vvenommm <96784333+vvenommm@users.noreply.github.com>
2022-06-24 17:29:12 +0900

    Create 0624_MyAwt.java

---
+++ day03/0624_MyAwt.java
@@ -0,0 +1,13 @@
+package day03;
+
+import java.awt.Frame;
+
+public class MyAwt {
+
+	public static void main(String[] args) {
+		// TODO Java의 GUI
+		Frame f = new Frame();
+		f.setSize(400, 400);
+		f.setVisible(true);
+	}
+}

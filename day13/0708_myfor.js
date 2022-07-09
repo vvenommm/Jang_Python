c007f3cb36949a86f21e4340e84e9c6e13cdfc78
vvenommm <96784333+vvenommm@users.noreply.github.com>
2022-07-09 23:34:28 +0900

    Create 0708_myfor.js

---
+++ day13/0708_myfor.js
@@ -0,0 +1,30 @@
+function add(a, b){
+	return a+b;
+}
+
+function min(a, b){
+	return a-b;
+}
+function mul(a, b){
+	return a*b;
+}
+function div(a, b){
+	return a/b;
+}
+
+function mod(a, b){
+	return a%b;
+}
+
+
+
+var sum = add(6, 7);
+console.log("sum", sum);
+sum = min(6, 7);
+console.log("sum", sum);
+sum = mul(6, 7);
+console.log("sum", sum);
+sum = div(6, 7);
+console.log("sum", sum);
+sum = mod(6, 7);
+console.log("sum", sum);

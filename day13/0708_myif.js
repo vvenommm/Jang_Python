52ed058a94bbd9851b9c13c54472d7528333ad8b
vvenommm <96784333+vvenommm@users.noreply.github.com>
2022-07-09 23:34:51 +0900

    Create 0708_myif.js

---
+++ day13/0708_myif.js
@@ -0,0 +1,13 @@
+var a = 92;
+
+if(a >= 90){
+	console.log("수")
+}else if(a >= 80){
+	console.log("우")
+}else if(a >= 70){
+	console.log("미")
+}else if(a >= 60){
+	console.log("미")
+}else{
+	console.log("가")
+}

20ccf493bec7e08d3f814a60f4ee9c0b0a530fdd
vvenommm <96784333+vvenommm@users.noreply.github.com>
2022-07-12 13:58:14 +0900

    Update 0711_expression01.js

--- day14_node.js/0711_expression01.js
+++ day14_node.js/0711_expression01.js
@@ -4,6 +4,13 @@
 const express = require('express');
 const app = express()
 const port = 3300;
+const http = require('http');
+const ejs = require('ejs');
+const server = http.createServer(app);
+
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended : true }));
 
 app.get('/', (req, res) => res.send("Hello World!"))
 
@@ -12,4 +19,19 @@ app.get('/para', (req, res) => {
 	console.log(req.query);
 })
 
+app.post('/post', (req, res) => {
+	var abcde = req.body.abcde
+	res.send('POST : ' + abcde)
+})
+
+
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+app.get('/ejs', (req, res) => {
+  res.render('abcd');
+})
+	
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+

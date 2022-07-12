00862531011376a2e90bc0210b8f2426c9e37617
vvenommm <96784333+vvenommm@users.noreply.github.com>
2022-07-12 17:25:26 +0900

    Update 0712_express02.js

--- day15_node.js/0712_express02.js
+++ day15_node.js/0712_express02.js
@@ -10,6 +10,8 @@ const http = require('http');
 const ejs = require('ejs');
 const server = http.createServer(app);
 
+var ep = new EmpDao();
+
 var bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended : true }));
@@ -19,10 +21,40 @@ app.set('views', './views');
 
 app.get('/', (req, res) => res.send("Hello World!"))
 app.get('/emp_list', (req, res) => {
-	var ep = new EmpDao();
 	let list = ep.selects();
-	
 	res.render("emp_list", {list : list});
+})
+
+app.get('/emp_detail', (req, res) => {
+	var e_id = req.query.e_id;
+	var emp = ep.select(e_id);
+	
+	res.render("emp_detail", {emp : emp});
+})
+
+app.get('/emp_mod', (req, res) => {
+	var e_id = req.query.e_id;
+	var emp = ep.select(e_id);
+	
+	res.render("emp_mod", {emp : emp});
+})
+
+app.post('/emp_mod_act', (req, res) => {
+	var e_id = req.body.e_id;
+	var e_name = req.body.e_name;
+	var addr = req.body.addr;
+	
+	var cnt = ep.update(e_id, e_name, addr);
+	
+	res.render("emp_mod_act", {cnt : cnt});
+})
+
+app.get('/emp_del_act', (req, res) => {
+	var e_id = req.query.e_id;
+	
+	var cnt = ep.delete(e_id);
+	
+	res.render("emp_del_act", {cnt : cnt});
 })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
